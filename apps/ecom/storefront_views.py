4198fbc38c3819a59969a690bb1530ad45a14e1a
Saleh Ahmad Shakil <shakil493888@gmail.com>
2026-02-06 02:52:43 +0600

    feat: Add product detail page with dynamic content and image gallery

--- apps/ecom/storefront_views.py
+++ apps/ecom/storefront_views.py
@@ -20,12 +20,22 @@ class ProductListView(TemplateView):
     template_name = 'storefront/product_list.html'
 
 
+from django.shortcuts import get_object_or_404
+from apps.ecom.models import Product
+
 class ProductDetailView(TemplateView):
     """
     Product detail page with variant selection and add to cart.
     Product data loaded via AJAX using slug from URL.
     """
     template_name = 'storefront/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = self.kwargs.get('slug')
+        product = get_object_or_404(Product, slug=slug, is_active=True)
+        context['product'] = product
+        return context
 
 
 class CartView(LoginRequiredMixin, TemplateView):
